f5ae5bdf06d410b44d4fad68bd6f4faddf94de7a
Ahmed Alhisan <alhisan.swe@gmail.com>
2023-01-11 23:28:36 +0300

    Final edit

--- Main.py
+++ Main.py
@@ -1,23 +1,16 @@
-def findprime(num1:int  , num2:int):
-        for i in range(num1 , num2 +1):
-            
-                if(i==1):
-                    continue
-                elif(i == 2 or i ==3 or i == 5 or i ==7 ):
-                    print(i)
-                elif (i % 2 != 0 and i % 3 !=0 and i % 5 !=0 and i % 7 !=0 and i % 9 !=0):
-                    print(i)
-                   
-        print("The End of Loop")
+def findprime(max:int ):
+    print(1)
+    for i in range(2 ,max+1):
+      prime = True
+      for j in range(2 , i):
+        num = i%j
+        if num == 0:
+          prime = False
+          break
+      if prime:
+        print(i)  
 
 
-
-findprime( int(input("please enter the first number")) , int(input("please enter the second number")) )        
-            
-            
-
-   
-
-
+findprime(50)        
     
-
+ 
