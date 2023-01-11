3aa938b95f807dcb66dfe48e5c51722ab5b90dda
Sara Alotaibi <alotaibisara49@gmail.com>
2023-01-11 18:45:46 +0300

    solved the lab :)

---
+++ lab function2.py
@@ -0,0 +1,10 @@
+def my_function(a , b)->int:
+    a = i
+    b= j
+    return
+for i in range(2,50): 
+    for j in range(2,50):
+       if i%j == 0:
+            break
+    if i == j:
+      print(i)
