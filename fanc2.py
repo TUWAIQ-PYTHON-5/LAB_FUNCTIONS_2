56dc662b1d2354bcac2e4886a5d8d0d112be245c
MOH7117 <mohammad201027@hotmail.com>
2023-01-11 17:59:47 +0300

    func2

--- fanc2.py
+++ fanc2.py
@@ -1,4 +1,4 @@
-def funk2 (num1,num2):
+def func2 (num1,num2):
     for num in range(num1,num2 +1):
         if num>num1 and num<num2 :
             for i in range(2,num):
@@ -6,4 +6,4 @@ def funk2 (num1,num2):
                     break
             else:
                 print(num)
-funk2(25,50) 
+func2(25,50) 
