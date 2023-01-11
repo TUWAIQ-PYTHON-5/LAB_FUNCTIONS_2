e168aeef43d75b25a110a4ac40e2708854403d0f
atheer <atheeralq0@gmail.com>
2023-01-11 17:17:01 +0300

    end the lab

---
+++ lab4.py
@@ -0,0 +1,11 @@
+def find_primes (num1 :int , num2 :int):
+    for numb_1 in range (num1,num2):
+        if numb_1 % 2 == 0:
+            continue
+        print(numb_1)
+
+
+
+
+find_primes(25,50)
+
