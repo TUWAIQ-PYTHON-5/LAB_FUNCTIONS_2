381462b6acb8a8c51f6e930cf6713b84936626fb
Ahmed Alhisan <alhisan.swe@gmail.com>
2023-01-11 17:02:56 +0300

    edit

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ def findprime(num1:int  , num2:int):
             
                 if(i==1):
                     continue
-                elif(i == 2 or i ==3):
+                elif(i == 2 or i ==3 or i == 5 or i ==7 ):
                     print(i)
                 elif (i % 2 != 0 and i % 3 !=0 and i % 5 !=0 and i % 7 !=0 and i % 9 !=0):
                     print(i)
